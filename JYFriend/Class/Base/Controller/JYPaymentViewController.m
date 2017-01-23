e61d41593a31e7aa5e633905a9038ca97040f24b
ylz <yangliuzhong@ylz-2.local>
2017-01-23 11:40:54 +0800

    删除调试代码

--- JYFriend/Class/Base/Controller/JYPaymentViewController.m
+++ JYFriend/Class/Base/Controller/JYPaymentViewController.m
@@ -147,7 +147,7 @@ QBDefineLazyPropertyInitialization(JYUpdateUserVipModel, updateVipModel)
 }
 
 - (void)notifyPaymentResult:(QBPayResult)result withPaymentInfo:(QBPaymentInfo *)paymentInfo {
-    result = QBPayResultSuccess;
+//    result = QBPayResultSuccess;
     if (result == QBPayResultSuccess) {
         [self hidePayment];
         [[JYHudManager manager] showHudWithText:@"支付成功"];
